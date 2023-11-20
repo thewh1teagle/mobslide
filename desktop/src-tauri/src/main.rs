15e6398953974e50881339c37ed82f4551305fcd
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-11-20 17:11:53 +0200

    update to latest enigo

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -1,34 +1,34 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-
 #[cfg(any(windows, target_os = "macos"))]
 use window_shadows::set_shadow;
 
+use enigo::{Direction, Enigo, Key, Keyboard, Settings};
 use tauri::Manager;
-use enigo::*;
 
 #[tauri::command]
 async fn press(key: String) -> Result<(), String> {
-    let mut controller = Enigo::new();
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+
     match key.as_str() {
         "VOL_UP" => {
-            controller.key_down(Key::VolumeUp);
-        },
+            enigo.key(Key::VolumeUp, Direction::Click).unwrap();
+        }
         "VOL_DN" => {
-            controller.key_down(Key::VolumeDown);
-        },
+            enigo.key(Key::VolumeDown, Direction::Click).unwrap();
+        }
         "PG_UP" => {
-            controller.key_down(Key::PageUp);
-        },
+            enigo.key(Key::PageUp, Direction::Click).unwrap();
+        }
         "PG_DN" => {
-            controller.key_down(Key::PageDown);
-        },
+            enigo.key(Key::PageDown, Direction::Click).unwrap();
+        }
         "F5" => {
-            controller.key_down(Key::F5);
-        },
+            enigo.key(Key::F5, Direction::Click).unwrap();
+        }
         "ESC" => {
-            controller.key_down(Key::Escape);
+            enigo.key(Key::Escape, Direction::Click).unwrap();
         }
         _ => {}
     }
@@ -37,15 +37,15 @@ async fn press(key: String) -> Result<(), String> {
 
 fn main() {
     tauri::Builder::default()
-    .setup(|app| {
-        let window = app.get_window("main").unwrap();
-        
-        #[cfg(any(windows, target_os = "macos"))]
-        set_shadow(&window, true).unwrap();
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
 
-        Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![press])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+            #[cfg(any(windows, target_os = "macos"))]
+            set_shadow(&window, true).unwrap();
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![press])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
